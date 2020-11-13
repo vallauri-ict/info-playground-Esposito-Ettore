ceb881915159fa1bca57fbb05f658a69878f7f22
Esposito-Ettore <62542043+Esposito-Ettore@users.noreply.github.com>
2020-11-13 08:52:52 +0100

    Compito

--- 03_01_FormRegEx/03_01_FormRegEx/GestioneUtenti.cs
+++ 03_01_FormRegEx/03_01_FormRegEx/GestioneUtenti.cs
@@ -11,12 +11,16 @@ namespace _03_01_FormRegEx
     class GestioneUtenti
     {
         private static GestioneUtenti ind = null;
-        private readonly string nomeFile = "Utenti.txt";
-        private readonly char div = '|';
+        private const string nomeFile = "Utenti.txt";
+        private const char div = '|';
+        private List<string> ListaUtenti = new List<string>();
 
         private GestioneUtenti()
         {
-
+            StreamReader sr = new StreamReader(nomeFile);
+            while (sr.Peek() != -1)
+                ListaUtenti.Add(sr.ReadLine());
+            sr.Close();
         }
 
         public static GestioneUtenti CreaGestione()
@@ -28,19 +32,19 @@ namespace _03_01_FormRegEx
 
         internal bool AggiungiUtente(string Cognome, string Nome, string Indirizzo, string Citta, string Cap, string Mail, string CodFisc, string User, string Password)
         {
-            StreamReader sr = new StreamReader(nomeFile);
             bool aggiunto = true;
-            while (sr.Peek() != -1 && aggiunto)
+            for (int i = 0; i < ListaUtenti.Count && aggiunto; i++)
             {
-                string userUtente = sr.ReadLine().Split(div)[7];
-                if (User == userUtente)
+                string UserAtt = ListaUtenti[i].Split(div)[7];
+                if (UserAtt == User)
                     aggiunto = false;
             }
-            sr.Close();
             if (aggiunto)
             {
+                string NuovoUtente = Cognome + div + Nome + div + Indirizzo + div + Citta + div + Cap + div + Mail + div + CodFisc + div + User + div + Password;
+                ListaUtenti.Add(NuovoUtente);
                 StreamWriter sw = new StreamWriter(nomeFile, true);
-                sw.WriteLine(Cognome + div + Nome + div + Indirizzo + div + Citta + div + Cap + div + Mail + div + CodFisc + div + User + div + Password);
+                sw.WriteLine(NuovoUtente);
                 sw.Close();
             }
             return aggiunto;
