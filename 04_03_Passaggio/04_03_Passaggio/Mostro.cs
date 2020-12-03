b5f26249336736ea6f3b3eeff9b20f973e2300fd
Esposito-Ettore <62542043+Esposito-Ettore@users.noreply.github.com>
2020-12-03 15:51:09 +0100

    compito

---
+++ 04_03_Passaggio/04_03_Passaggio/Mostro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_03_Passaggio
+{
+    public class Mostro
+    {
+        public string nome;
+        public Mostro() : this("Mostro")
+        {
+
+        }
+
+        public Mostro(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public void Rivela()
+        {
+            MessageBox.Show(nome);
+        }
+    }
+}
